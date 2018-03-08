1885d514252f500f86434a49583a50307ed836ca
ZHENGXIN\pc <talentzzx@gmail.com>
2018-03-08 22:42:55 +0800

    model: add duplicateClientException class

---
+++ src/main/java/seedu/address/model/client/exceptions/DuplicateClientException.java
@@ -0,0 +1,12 @@
+package seedu.address.model.client.exceptions;
+
+import seedu.address.commons.exceptions.DuplicateDataException;
+
+/**
+ * Signals that the operation will result in duplicate Client objects.
+ */
+public class DuplicateClientException extends DuplicateDataException {
+    public DuplicateClientException() {
+        super("Operation would result in duplicate persons");
+    }
+}
