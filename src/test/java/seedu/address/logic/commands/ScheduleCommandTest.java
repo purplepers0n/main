29f9499da5f25f6026255447b53b8d10ca0dc081
ZHENGXIN\pc <talentzzx@gmail.com>
2018-03-14 23:28:08 +0800

    add ScheduleCommand test

---
+++ src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.commands;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScheduleCommandTest {
+
+    @Test
+    public void executeUndoableCommand() throws Exception{
+    }
+}
