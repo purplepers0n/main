a99fc59835a3a7b03442abc4abce77a531d09b70
ZHENGXIN\pc <talentzzx@gmail.com>
2018-03-15 00:34:39 +0800

    add ScheduleCommandParser

---
+++ src/main/java/seedu/address/logic/parser/ScheduleCommandParser.java
@@ -0,0 +1,44 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.AddCommandParser.arePrefixesPresent;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PERSON_ROLE;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.ScheduleCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.appointment.Appointment;
+import seedu.address.model.appointment.Date;
+import seedu.address.model.appointment.Time;
+
+/**
+ * Parses input arguments and creates a new {@code ScheduleCommand} object
+ */
+public class ScheduleCommandParser implements Parser<ScheduleCommand> {
+    /**
+     * Parses the given {@code String} of arguments in the context of the {@code ScheduleCommand}
+     * and returns a {@code ScheduleCommand} object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public ScheduleCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_TIME, PREFIX_DATE);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_TIME, PREFIX_DATE)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ScheduleCommand.MESSAGE_USAGE));
+        }
+        try {
+            Date date = ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE)).get();
+            Time time = ParserUtil.parseTime(argMultimap.getValue(PREFIX_TIME)).get();
+
+            Appointment appointment = new Appointment(date, time);
+
+        } catch (IllegalValueException ive) {
+            throw new ParseException(ive.getMessage(), ive);
+        }
+
+        return new ScheduleCommand(appointment);
+    }
+}
