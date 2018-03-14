ca5b66dd9f46cece975bd66cb8e8c0510361ee72
ZHENGXIN\pc <talentzzx@gmail.com>
2018-03-14 23:46:06 +0800

    add 2 arguments to ScheduleCommand

--- src/main/java/seedu/address/logic/commands/ScheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/ScheduleCommand.java
@@ -1,6 +1,10 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.appointment.Date;
+import seedu.address.model.appointment.Time;
 
 /**
  * Schedule the date and time for an appointment
@@ -9,6 +13,20 @@ public class ScheduleCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "schedule";
 
+    private final Time time;
+    private final Date date;
+
+    /**
+     * @param time of the appointment to be scheduled
+     * @param date of the appointment to be scheduled
+     */
+    public ScheduleCommand(Time time, Date date) {
+        requireNonNull(time);
+        requireNonNull(date);
+
+        this.time = time;
+        this.date = date;
+    }
     @Override
     protected CommandResult executeUndoableCommand() throws CommandException {
         return null;
