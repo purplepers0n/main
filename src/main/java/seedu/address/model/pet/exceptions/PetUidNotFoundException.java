79f230a8036dc168e808e27ceabc870eb00ab207
jonathanwj <jonathanwj@users.noreply.github.com>
2018-03-14 14:17:27 +0800

    UniquePetUidList: Add UniquePetUidList class for list of pet uids

---
+++ src/main/java/seedu/address/model/pet/exceptions/PetUidNotFoundException.java
@@ -0,0 +1,7 @@
+package seedu.address.model.pet.exceptions;
+
+/**
+ * Signals the operation is unable to find the specified petUid.
+ */
+public class PetUidNotFoundException extends Exception {
+}
