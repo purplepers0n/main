b3e4f7d35fc7306158e87e5fc6a39d15560e5615
ZHENGXIN\pc <talentzzx@gmail.com>
2018-03-16 01:55:29 +0800

    AppointmentUtil.java: add

---
+++ src/test/java/seedu/address/testutil/AppointmentUtil.java
@@ -0,0 +1,32 @@
+package seedu.address.testutil;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
+
+import seedu.address.logic.commands.ScheduleCommand;
+import seedu.address.model.appointment.Appointment;
+
+/**
+ * A utility class for Appointment.
+ */
+public class AppointmentUtil {
+
+    /**
+     * Returns an schedule command string for scheduling the {@code appointment}.
+     */
+    public static String getScheduleCommand(Appointment appointment) {
+        return ScheduleCommand.COMMAND_WORD + " " + getAppointmentDetails(appointment);
+    }
+
+    /**
+     * Returns the part of command string for the given {@code appointment}'s details.
+     */
+    public static String getAppointmentDetails(Appointment appointment) {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(PREFIX_DATE + appointment.getDate().toString() + " ");
+        sb.append(PREFIX_TIME + appointment.getTime().toString() + " ");
+
+        return sb.toString();
+    }
+}
