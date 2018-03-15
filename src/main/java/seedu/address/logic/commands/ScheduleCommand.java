7cec52f09a5c671e76e131ff0fa8e39d17fa9723
ZHENGXIN\pc <talentzzx@gmail.com>
2018-03-15 14:58:21 +0800

    add message to ScheduleCommand

--- src/main/java/seedu/address/logic/commands/ScheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/ScheduleCommand.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;
 
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.appointment.Appointment;
@@ -11,8 +13,16 @@ import seedu.address.model.appointment.Appointment;
 public class ScheduleCommand extends UndoableCommand {
 
     public static final String COMMAND_WORD = "schedule";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Schedule a date and time for an appointment. "
+            + "Parameters: "
+            + PREFIX_DATE + "DATE "
+            + PREFIX_TIME + "TIME\n"
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_DATE + "01/05/2012 "
+            + PREFIX_TIME + "1500 ";
 
     private final Appointment toAdd;
+
     /**
      * Creates an ScheduleCommand to add the specified {@code appointment}
      */
