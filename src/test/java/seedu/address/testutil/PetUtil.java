7046b7980f60e40b7c20d2dac769e2905bd9a85b
Md Azhar <md.azsa@gmail.com>
2018-03-16 16:11:22 +0800

    Adds PetUtil

--- src/test/java/seedu/address/testutil/PetUtil.java
+++ src/test/java/seedu/address/testutil/PetUtil.java
@@ -1,5 +1,10 @@
 package seedu.address.testutil;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PET_AGE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PET_GENDER;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PET_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
 import seedu.address.logic.commands.AddPetCommand;
 import seedu.address.model.pet.Pet;
 
@@ -19,6 +24,13 @@ public class PetUtil {
      * Returns the details of the pet
      */
     public static String getPetDetails(Pet pet) {
-        
+        StringBuilder sb = new StringBuilder();
+        sb.append(PREFIX_PET_NAME + pet.getPetName().toString());
+        sb.append(PREFIX_PET_AGE + pet.getPetAge().value);
+        sb.append(PREFIX_PET_GENDER + pet.getPetGender().toString());
+        pet.getTags().stream().forEach(
+            s -> sb.append(PREFIX_TAG + s.tagName + " ")
+        );
+        return sb.toString();
     }
 }
