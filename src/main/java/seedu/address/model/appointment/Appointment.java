ca19cc992bba643d8a6c3197be47f65217fe73bc
ZHENGXIN\pc <talentzzx@gmail.com>
2018-03-21 21:38:52 +0800

    add duration in appointment

--- src/main/java/seedu/address/model/appointment/Appointment.java
+++ src/main/java/seedu/address/model/appointment/Appointment.java
@@ -12,15 +12,16 @@ public class Appointment {
 
     private final Date date;
     private final Time time;
+    private final Duration duration;
 
     /**
      * Every field must be present and not null.
      */
-    public Appointment(Date date, Time time) {
+    public Appointment(Date date, Time time, Duration duration) {
         requireAllNonNull(date, time);
         this.date = date;
         this.time = time;
-
+        this.duration = duration;
     }
 
     public Date getDate() {
@@ -29,6 +30,10 @@ public class Appointment {
 
     public Time getTime() {
         return time;
+    }
+
+    public Duration getDuration() {
+        return duration;
     }
 
     @Override
@@ -50,7 +55,7 @@ public class Appointment {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(date, time);
+        return Objects.hash(date, time, duration);
 
     }
 
@@ -61,7 +66,9 @@ public class Appointment {
         builder.append(" Date: ")
                 .append(getDate())
                 .append(" Time: ")
-                .append(getTime());
+                .append(getTime())
+                .append(" Duration: ")
+                .append(getDuration());
 
         return builder.toString();
     }
