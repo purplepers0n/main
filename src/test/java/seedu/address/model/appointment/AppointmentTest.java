1366fa2d409af28c3c51804fe092e0a6ffc19c83
ZHENGXIN\pc <talentzzx@gmail.com>
2018-03-22 20:11:37 +0800

    appt test update

--- src/test/java/seedu/address/model/appointment/AppointmentTest.java
+++ src/test/java/seedu/address/model/appointment/AppointmentTest.java
@@ -9,13 +9,14 @@ public class AppointmentTest {
 
     @Test
     public void equals() {
-        Appointment appointment1 = new Appointment(new Date("2018-01-01"), new Time("14:00"));
+        Appointment appointment1 = new Appointment(new Date("2018-01-01"), new Time("14:00"), new Duration("30"));
 
         // same object -> returns true
         assertTrue(appointment1.equals(appointment1));
 
         // same values -> returns true
-        Appointment appointment1Copy = new Appointment(appointment1.getDate(), appointment1.getTime());
+        Appointment appointment1Copy = new Appointment(appointment1.getDate(), appointment1.getTime(),
+                appointment1.getDuration());
         assertTrue(appointment1.equals(appointment1Copy));
 
         // different types -> returns false
@@ -25,8 +26,22 @@ public class AppointmentTest {
         assertFalse(appointment1.equals(null));
 
         // different appointment -> returns false
-        Appointment differentAppointment = new Appointment(new Date("2018-11-20"), new Time("15:15"));
-        assertFalse(appointment1.equals(differentAppointment));
+        Appointment differentAppointmentWithDifferentDuration = new Appointment(new Date("2018-11-20"),
+                new Time("15:15"), new Duration("100"));
+        assertFalse(appointment1.equals(differentAppointmentWithDifferentDuration));
+
+        Appointment differentAppointmentWithSameDuration = new Appointment(new Date("2018-11-20"),
+                new Time("15:15"), new Duration("30"));
+        assertFalse(appointment1.equals(differentAppointmentWithSameDuration));
+
+        //duplicate appointment -> returns true
+        Appointment duplicateAppointmentWithDifferentDuration = new Appointment(new Date("2018-01-01"),
+                new Time("14:00"), new Duration("100"));
+        assertTrue(appointment1.equals(duplicateAppointmentWithDifferentDuration));
+
+        Appointment duplicateAppointmentWithSameDuration = new Appointment(new Date("2018-01-01"),
+                new Time("14:00"), new Duration("30"));
+        assertTrue(appointment1.equals(duplicateAppointmentWithSameDuration));
     }
 
 }
