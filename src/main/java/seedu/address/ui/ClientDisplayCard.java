e19ff532c7319b16a7125d6c79abdb9040a00493
purplepers0n <choo.wx96@gmail.com>
2018-04-08 17:18:22 +0800

    for client display in listallpanel section (like client card but without index)

---
+++ src/main/java/seedu/address/ui/ClientDisplayCard.java
@@ -0,0 +1,88 @@
+package seedu.address.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.client.Client;
+
+//@@author purplepers0n-reused
+/**
+ * A UI component that displays information of a {@code Client} for {@code ListAllPanel} display.
+ */
+public class ClientDisplayCard extends UiPart<Region> {
+    private static final String FXML = "ClientDisplayCard.fxml";
+    
+    private static final String[] TAG_COLOR = {"red", "yellow", "blue", "orange", "green",
+            "pink", "navy", "teal", "purple", "peach", "lightblue", "darkpurple",
+            "green2", "white", "wine", "fuchsia", "sea"};
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final Client client;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private Label phone;
+    @FXML
+    private Label address;
+    @FXML
+    private Label email;
+    @FXML
+    private FlowPane tags;
+
+    public ClientDisplayCard(Client client) {
+        super(FXML);
+        this.client = client;
+        name.setText(client.getName().fullName);
+        phone.setText(client.getPhone().value);
+        address.setText(client.getAddress().value);
+        email.setText(client.getEmail().value);
+        initTags(client);
+    }
+
+    /**
+     * @return the color for {@code tagName}'s label
+     */
+    private String getTagColorFor(String tagName) {
+        return TAG_COLOR[Math.abs(tagName.hashCode()) % TAG_COLOR.length];
+    }
+
+    /**
+     * Creates the tag labels for {@code client}.
+     */
+    private void initTags(Client client) {
+        client.getTags().forEach(tag -> {
+            Label tagLabel = new Label(tag.tagName);
+            tagLabel.getStyleClass().add(getTagColorFor(tag.tagName));
+            tags.getChildren().add(tagLabel);
+        });
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ClientCard)) {
+            return false;
+        }
+
+        // state check
+        ClientCard card = (ClientCard) other;
+        return client.equals(card.client);
+    }
+}
